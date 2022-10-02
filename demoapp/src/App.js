42b598f8c59c7f9f654dd11b46f05c2fde375654
sumith indika <tsumith44@gmail.com>
2022-10-02 16:38:38 +0530

    add to project

---
+++ demoapp/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.scss';
+import Profile from '../src/components/profile/Profile';
+function App() {
+  return (
+    <div>
+      <Profile/>
+    </div>
+  );
+}
+
+export default App;
